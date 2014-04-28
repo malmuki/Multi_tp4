693baa671fb56b8a7fd767b1e27cbbaea46d9045
fdelaney <delaneyfrancis@hotmail.com>
2014-04-28 09:53:35 -0400

    Update Francis!
    Updated DB
    Adding changementD'avatar
    adding News Feed

---
+++ pageActualite.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="pageActualite.aspx.cs" Inherits="pageActualite" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="Server">
+    <h1>NEWS!</h1>
+    <asp:Table runat="server" ID="tableActualite">
+        <asp:TableHeaderRow>
+            <asp:TableHeaderCell>Auteur</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Message</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Date de creation</asp:TableHeaderCell>
+        </asp:TableHeaderRow>
+    </asp:Table>
+
+    <%if(Session["id"] != null && IsModerator()){%>
+    <asp:TextBox TextMode="MultiLine" runat="server" ID="txtActualite"></asp:TextBox>
+    <asp:Button runat="server" ID="btnEnvoyerMessage" Text="Envoyer" OnClick="btnEnvoyerMessage_Click"/>
+    <%}else{%>
+    <%}%>
+</asp:Content>
+
---
+++ pageActualite.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Configuration;
+using System.Web.UI.HtmlControls;
+
+public partial class pageActualite : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string ID = Request["id"];
+        string nomSujet = Request["titre"];
+
+        OleDbConnection connection = new OleDbConnection(ConfigurationManager.ConnectionStrings["GeneralDatabase"].ConnectionString);
+        connection.Open();
+
+        OleDbCommand command = new OleDbCommand("SELECT messages.message, messages.date_ecriture, messages.auteur, Utilisateurs.nom_fichier_avatar FROM Utilisateurs INNER JOIN messages ON Utilisateurs.[nom_utilisateur] = messages.[auteur] WHERE sujet = 1 ORDER BY messages.date_ecriture;", connection);
+        command.Parameters.Add(new OleDbParameter("id", ID) { OleDbType = OleDbType.VarChar, Size = 255 });
+        OleDbDataReader datareader = command.ExecuteReader();
+
+        while (datareader.Read())
+        {
+            TableRow tableRow = new TableRow();
+            TableCell userCell = new TableCell();
+            HtmlGenericControl imgDiv = new HtmlGenericControl("div");
+            HtmlGenericControl pseudoDiv = new HtmlGenericControl("div");
+
+            Image avatar = new Image();
+            avatar.ImageUrl = "~/App_Data/image/" + (string)datareader[3];
+            imgDiv.Controls.Add(avatar);
+            pseudoDiv.InnerText = (string)datareader[2];
+            userCell.Controls.Add(imgDiv);
+            userCell.Controls.Add(pseudoDiv);
+
+            tableRow.Cells.Add(userCell);
+            tableRow.Cells.Add(new TableCell() { Text = datareader.IsDBNull(0) ? "" : ((string)datareader[0]) });
+            tableRow.Cells.Add(new TableCell() { Text = datareader.IsDBNull(1) ? "" : ((DateTime)datareader[1]).ToLongTimeString() });
+            tableActualite.Rows.Add(tableRow);
+        }
+        connection.Close();
+    }
+
+   public bool IsModerator()
+    {
+         OleDbConnection connection = new OleDbConnection(ConfigurationManager.ConnectionStrings["GeneralDatabase"].ConnectionString);
+        connection.Open();
+
+        OleDbCommand command = new OleDbCommand("SELECT moderateur FROM Utilisateur WHERE nom_utilisateur="+Session["id"], connection);
+        OleDbDataReader datareader = command.ExecuteReader();
+
+        return (bool)datareader[0];
+    }
+   protected void btnEnvoyerMessage_Click(object sender, EventArgs e)
+   {
+       OleDbConnection connection = new OleDbConnection(ConfigurationManager.ConnectionStrings["database"].ConnectionString);
+        connection.Open();
+
+        OleDbCommand command = new OleDbCommand("INSERT INTO messages (sujet, auteur, date_ecriture, message) VALUES (@sujet, @auteur, @date, @message)", connection);
+
+        command.Parameters.Add(new OleDbParameter("sujet", 1)
+        {
+            OleDbType = OleDbType.Integer
+        });
+
+
+        command.Parameters.Add(new OleDbParameter("auteur", Session["id"])
+        {
+            OleDbType = OleDbType.VarChar, Size = 255
+        });
+
+        command.Parameters.Add(new OleDbParameter("date", DateTime.Now)
+        {
+            OleDbType = OleDbType.Date
+        });
+
+
+        command.Parameters.Add(new OleDbParameter("message", txtActualite.Text)
+        {
+            OleDbType = OleDbType.VarChar, Size = 65536 //Changer le nombre de caracteres pour un text long.
+        });
+
+        command.ExecuteNonQuery();
+        connection.Close();
+   }
+}
