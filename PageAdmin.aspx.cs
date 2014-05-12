da38762859fe8f371b9d535fa0192e7125a309ab
Francois Chantal <geniousmouton@gmail.com>
2014-05-12 11:42:56 -0400

    1.3.1

--- PageAdmin.aspx.cs
+++ PageAdmin.aspx.cs
@@ -31,9 +31,18 @@ public partial class Default2 : System.Web.UI.Page
         OleDbConnection connection = new OleDbConnection(ConfigurationManager.ConnectionStrings["GeneralDatabase"].ConnectionString);
         connection.Open();
 
-        OleDbCommand command = new OleDbCommand("SELECT administrateur FROM Utilisateur WHERE nom_utilisateur=" + Session["id"], connection);
+        OleDbCommand command = new OleDbCommand("SELECT administrateur FROM Utilisateurs WHERE nom_utilisateur = @sessionID", connection);
+        command.Parameters.Add(new OleDbParameter("sessionID", (string)Session["id"]) { OleDbType = OleDbType.VarChar, Size = 255 });
         OleDbDataReader datareader = command.ExecuteReader();
 
-        return (bool)datareader[0];
+        if (datareader.Read())
+        {
+            return (bool)datareader[0];
+        }
+        else
+        {
+            return false;
+        }
+
     }
 }
